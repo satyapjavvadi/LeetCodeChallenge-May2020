ff93a772b2da408506e3c8acf2c6bd6f2ee0e107
Satya P Javvadi <saphin.rao@gmail.com>
2020-05-02 22:19:36 -0700

    Added FirstBadVersion progam

---
+++ LeetCodeChallenge-May2020/FindBadVersion/FindBadVersion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeChallenge_May2020
+{
+    class FindBadVersion
+    {
+        public static void Test()
+        {
+            int[] arr = { 1, 2, 3, 4, 5};
+            int n = arr.Length;
+            
+        }
+
+        public static int FirstBadVersion(int[] arr)
+        {
+
+            int low = 1;
+            int high = arr.Length;
+
+
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                if (arr[mid] == mid)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return low;
+        }
+    }
+}
--- LeetCodeChallenge-May2020/JewelsInStones/JewelsInStones.cs
+++ LeetCodeChallenge-May2020/JewelsInStones/JewelsInStones.cs
@@ -14,20 +14,53 @@ namespace LeetCodeChallenge_May2020
 
             Console.WriteLine(count);
         }
+
+        // Brute force approach
+        /*   public static int NumJewelsInStones(string J, string S)
+             {
+                 char[] chJ = J.ToCharArray();
+                 char[] chS = S.ToCharArray();
+                 int count = 0;
+
+                 for (int i = 0; i < chJ.Length; i++)
+                 {
+                     for (int j = 0; j < chS.Length; j++)
+                     {
+                         if (chJ[i] == chS[j])
+                         {
+                             count++;
+                         }
+                     }
+                 }
+                 return count;
+             }  */
+
+
+        //Using Dictionary
+
         public static int NumJewelsInStones(string J, string S)
         {
             char[] chJ = J.ToCharArray();
             char[] chS = S.ToCharArray();
             int count = 0;
+            Dictionary<char, int> dict = new Dictionary<char, int>();
 
             for (int i = 0; i < chJ.Length; i++)
             {
-                for (int j = 0; j < chS.Length; j++)
+                if (!dict.ContainsKey(chJ[i]))
                 {
-                    if (chJ[i] == chS[j])
-                    {
-                        count++;
-                    }
+                    dict.Add(chJ[i], 1);
+                }
+                else
+                {
+                    dict[chJ[i]]++;
+                }
+            }
+            for (int j = 0; j < chS.Length; j++)
+            {
+                if (dict.ContainsKey(chS[j]))
+                {
+                    count++;
                 }
             }
             return count;
