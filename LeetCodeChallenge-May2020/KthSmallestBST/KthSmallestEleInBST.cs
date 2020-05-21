19421d8298bdad91dea8f20a7a35ae7bd5a92778
Satya Javvadi <saphin.rao@outlook.com>
2020-05-20 23:52:20 -0700

    Added KthSmallestElement in BST program

---
+++ LeetCodeChallenge-May2020/KthSmallestBST/KthSmallestEleInBST.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class KthSmallestEleInBST
+{
+    public class TreeNode
+    {
+      public int val;
+      public TreeNode left;
+         public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+ 
+    }
+    public static int KthSmallest(TreeNode root, int k)
+    {
+
+        Stack<TreeNode> st = new Stack<TreeNode>();
+
+        TraverseLeft(st, root);
+        while (--k > 0)
+        {
+            TraverseLeft(st, st.Pop().right);
+
+        }
+        return st.Pop().val;
+    }
+
+    void TraverseLeft(Stack<TreeNode> st, TreeNode root)
+    {
+        while (root != null)
+        {
+            st.Push(root);
+            root = root.left;
+        }
+    }
+}
